0564d02337ba5bb3dfd7c54ea1c7b4ef1138477c
Michal Tesler <michal.tesler@gmail.com>
2022-09-19 07:06:36 +0200

    Download list of s&P500 companies

---
+++ eda_sp500.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import streamlit as st
+
+
+def load_data():
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    html = pd.read_html(url, header=0)
+    df = html[0]
+    return df
+
+
+df = load_data()
+print(df)
