447200806625ba25bf977e9d2715a2433ae42489
Michal Tesler <michal.tesler@gmail.com>
2022-09-30 07:11:04 +0200

    Add default value to the sidebar

--- eda_sp500.py
+++ eda_sp500.py
@@ -28,7 +28,8 @@ sector = df.groupby('GICS Sector')
 
 # Sidebar - Sector selection
 sorted_sector_unique = sorted(df['GICS Sector'].unique())
-selected_sector = st.sidebar.multiselect('Sector', sorted_sector_unique)
+selected_sector = st.sidebar.multiselect(
+    'Sector', sorted_sector_unique, default=sorted_sector_unique)
 
 # Filtering data
 df_selected_sector = df[(df['GICS Sector'].isin(selected_sector))]
